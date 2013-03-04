b7899f0b68a8f020a1cdcdcef047c6216cc5c17a
Hunter C <hmclaug@gmail.com>
2013-03-04 15:32:23 -0600

    Added basic functionality to UI
    Added Framework

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -3,57 +3,76 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
+	<title>Intervention Thingy</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('laravel/css/style.css') }}
 	{{ Asset::container('bootstrapper')->styles() }}
+    
 </head>
-<body>
-	<div class="wrapper">
-		<header>
-			<h1>Laravel</h1>
-			<h2>A Framework For Web Artisans</h2>
+<body><div class="navbar navbar-inverse navbar-fixed-top">
+          <div class="navbar-inner">
+            <div class="container">
+              <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a href="/" class="brand">Intervention Thingy</a>
+              <div class="nav-collapse collapse">
+                <ul class="nav  pull-left">
+                  <li><a href="#">My Classes</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Manage Class <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#"><i class="icon-arrow-up icon-white"></i> Students</a></li>
+                      <li><a href="#">Interventions</a></li>
+                    </ul>
+                  </li>
+                  </ul>
+                  <ul class="nav  pull-right">
+                    <li><a href="#"><i class="icon-arrow-up icon-white"></i> Parents</a></li>
+                    <li><a href="#">Sign in</a></li>
+                  </ul> <!-- .nav -->
+              </div><!--/.nav-collapse -->
+            </div>
+          </div>
+        </div>
 
-			<p class="intro-text" style="margin-top: 45px;">
-			</p>
-		</header>
-		<div role="main" class="main">
-			<div class="home">
-				<h2>Learn the terrain.</h2>
 
-				<p>
-					You've landed yourself on our default home page. The route that
-					is generating this page lives at:
-				</p>
 
-				<pre>{{ path('app') }}routes.php</pre>
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span3">
+    	<ul class="nav nav-list">
+          <li class="nav-header">List header</li>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Students</a></li>
+          <li><a href="#">Compare</a></li>
+          <li><a href="#">Schedule</a></li>
+          <li><a href="#">Absences</a></li>
+          <li><a href="#">Assignments</a></li>
+                  <li><a href="#">Missing Assignments</a></li>
+                  <li><a href="#">Late Assignments</a></li>
+          <li><a href="#">Discipline</a></li>
+		</ul>
 
-				<p>And the view sitting before you can be found at:</p>
-
-				<pre>{{ path('app') }}views/home/index.blade.php</pre>
-
-				<h2>Grow in knowledge.</h2>
-
-				<p>
-					Learning to use Laravel is amazingly simple thanks to
-					its {{ HTML::link('docs', 'wonderful documentation') }}.
-				</p>
-
-				<h2>Create something beautiful.</h2>
-
-				<p>
-					Now that you're up and running, it's time to start creating!
-					Here are some links to help you get started:
-				</p>
-
-				<ul class="out-links">
-					<li><a href="http://laravel.com">Official Website</a></li>
-					<li><a href="http://forums.laravel.com">Laravel Forums</a></li>
-					<li><a href="http://github.com/laravel/laravel">GitHub Repository</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+    </div>
+    <div class="span9">
+      <!--Body content-->
+    </div>
+  </div>
+</div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	{{ Asset::container('bootstrapper')->scripts() }}
 </body>
 </html>
