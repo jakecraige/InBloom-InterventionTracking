527c51f81bb9e40d28823482111cab5d76bc6d5e
Hunter Skrasek <hunterskrasek@me.com>
2013-03-05 14:57:30 -0600

    Remove unneeded class option

--- sidebar.php
+++ sidebar.php
@@ -19,7 +19,7 @@ $sectionsURL = substr($sectionsURL[1], 1);
 $sections = $api->execute($sectionsURL);
 
 print "\t\t\t<div class='span3'>";
-print "\t\t\t\t<ul class='sidebar-nav unstyled' style='position: fixed;''>";
+print "\t\t\t\t<ul class='sidebar-nav unstyled' style='position: fixed;>";
 
     foreach ($sections as $section) 
     {
@@ -34,7 +34,6 @@ print "\t\t\t\t<ul class='sidebar-nav unstyled' style='position: fixed;''>";
       print "\t\t\t\t\t\t\t\t<ul class='unstyled'>\n";
       print sprintf("\t\t\t\t\t\t\t\t\t<li><a href='students.php?sectionId=%s'><i class='icon-th'></i>Students</a></li>\n",$section->id);
       print "\t\t\t\t\t\t\t\t\t<li><a href='#'><i class='icon-book'></i>Grades </a><span class='label label-important'>NYI</span></li>\n";
-      print "\t\t\t\t\t\t\t\t\t<li><a href='#'>Compare </a><span class='label label-important'>NYI</span></li>\n";
       print "\t\t\t\t\t\t\t\t</ul>\n";
       print "\t\t\t\t\t\t\t\t</div>\n";
       print "\t\t\t\t\t\t</div>\n";
