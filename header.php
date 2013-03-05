e4d836fb98b7a2ee515c14189eefff4f3022bf3b
Hunter Skrasek <hunterskrasek@me.com>
2013-03-05 12:17:03 -0600

    Update header for shadowbox and footer

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include_once 'settings.php';
 include_once 'api.php';
 
@@ -20,53 +19,91 @@ if ($json->code == '401') {
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="http://hunterskrasek.com/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://www.cs.stedwards.edu/~sazua/cosc4157/shadowbox-3.0.3/shadowbox-3.0.3/shadowbox.css" type="text/css">
   <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <script src="http://www.cs.stedwards.edu/~sazua/cosc4157/shadowbox-3.0.3/shadowbox-3.0.3/shadowbox.js"></script>
   <script src="http://twitter.github.com/bootstrap/assets/js/bootstrap.js"></script>
+  <script type="text/javascript">Shadowbox.init();</script>
+
+  <style type="text/css">
+      html, body {
+        height: 100%;
+        /* The html and body elements cannot have any padding or margin. */
+      }
+
+      /* Wrapper for page content to push down footer */
+      #wrap {
+        min-height: 100%;
+        height: auto !important;
+        height: 100%;
+        /* Negative indent footer by it's height */
+        margin: 0 auto -60px;
+      }
+
+      #push,
+      #footer {
+        height: 60px;
+      }
+      #footer {
+        background-color: #000;
+      }
+
+      /* Lastly, apply responsive CSS fixes as necessary */
+      @media (max-width: 767px) {
+        #footer {
+          margin-left: -20px;
+          margin-right: -20px;
+          padding-left: 20px;
+          padding-right: 20px;
+        }
+      }
+  </style>
 </head>
 <body>
-  <div class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-      <div class="container">
-        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a href="/" class="brand">Intervention Thingy <span class="label label-important">Alpha</span></a>
-        <div class="nav-collapse collapse">
-          <ul class="nav  pull-left">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-th-list"></i> My Classes <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#"><i class="icon-user"></i> Attendance</a></li>
-                <li><a href="#"><i class="icon-check"></i> Participation</a></li>
-              </ul>
-            </li>
+  <div id="wrap">
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="/" class="brand">Intervention Thingy <span class="label label-important">Alpha</span></a>
+          <div class="nav-collapse collapse">
+            <ul class="nav  pull-left">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-th-list"></i> My Classes <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#"><i class="icon-user"></i> Attendance</a></li>
+                  <li><a href="#"><i class="icon-check"></i> Participation</a></li>
+                </ul>
+              </li>
 
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-pencil"></i> Manage Class <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#"><i class="icon-arrow-up"></i> Students</a></li>
-                <li><a href="#">Interventions</a></li>
-              </ul>
-            </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-pencil"></i> Manage Class <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#"><i class="icon-arrow-up"></i> Students</a></li>
+                  <li><a href="#"><i class="icon-italic"></i> Interventions</a></li>
+                </ul>
+              </li>
 
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-calendar"></i>Schedule <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#"><i class="icon-user"></i> Parent Teacher Conference</a></li>
-                <li><a href="#">Office Hours</a></li>
-                <li><a href="#">In Class</a></li>
-                <li><a href="#">Detentions</a></li>
-              </ul>
-            </li>
-          </ul>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-calendar"></i>Schedule <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#"><i class="icon-user"></i> Parent Teacher Conference</a></li>
+                  <li><a href="#"><i class="icon-time"></i> Office Hours</a></li>
+                  <li><a href="#"><i class="icon-home"></i> In Class</a></li>
+                  <li><a href="#"><i class="icon-lock"></i> Detentions</a></li>
+                </ul>
+              </li>
+            </ul>
 
-          <ul class="nav pull-right">
-            <li><a href="#"><i class="icon-envelope"></i> Notifications</a></li>
-            <li><?php print '<a href="#">'.$json->full_name.'</a>'; ?></li>
-          </ul> <!-- .nav -->
-        </div><!--/.nav-collapse -->
+            <ul class="nav pull-right">
+              <li><a href="#"><i class="icon-envelope"></i> Notifications</a></li>
+              <li><?php print '<a href="#">'.$json->full_name.'</a>'; ?></li>
+            </ul> <!-- .nav -->
+          </div><!--/.nav-collapse -->
+        </div>
       </div>
     </div>
-  </div>
