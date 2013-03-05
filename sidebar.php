bcf9e385758d9d4e85fa908a52d061540a27a664
Hunter Skrasek <hunterskrasek@me.com>
2013-03-05 12:18:09 -0600

    Adjust sidebar for source output

--- sidebar.php
+++ sidebar.php
@@ -18,28 +18,29 @@ $sectionsURL = substr($sectionsURL[1], 1);
 
 $sections = $api->execute($sectionsURL);
 
+print "\t\t\t<div class='span3'>";
+print "\t\t\t\t<ul class='sidebar-nav unstyled' style='position: fixed;''>";
+
+    foreach ($sections as $section) 
+    {
+      print "\t\t\t\t\t<div class='accordion-group'>\n";
+      print "\t\t\t\t\t\t<div class='accordion-heading'>\n";
+      print sprintf("\t\t\t\t\t\t\t<a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion1'  href='#collapse%s'>\n",$section->id);
+      print "\t\t\t\t\t\t\t\t" . $section->uniqueSectionCode . "\n";
+      print "\t\t\t\t\t\t\t</a>\n";
+      print "\t\t\t\t\t\t</div>\n";
+      print sprintf("\t\t\t\t\t\t<div id='collapse%s' class='accordion-body collapse'>\n",$section->id);
+      print "\t\t\t\t\t\t\t<div class='accordion-inner'>\n";
+      print "\t\t\t\t\t\t\t\t<ul class='unstyled'>\n";
+      print sprintf("\t\t\t\t\t\t\t\t\t<li><a href='students.php?sectionId=%s'><i class='icon-th'></i>Students</a></li>\n",$section->id);
+      print "\t\t\t\t\t\t\t\t\t<li><a href='#'><i class='icon-book'></i>Grades </a><span class='label label-important'>NYI</span></li>\n";
+      print "\t\t\t\t\t\t\t\t\t<li><a href='#'>Compare </a><span class='label label-important'>NYI</span></li>\n";
+      print "\t\t\t\t\t\t\t\t</ul>\n";
+      print "\t\t\t\t\t\t\t\t</div>\n";
+      print "\t\t\t\t\t\t</div>\n";
+      print "\t\t\t\t\t</div>\n";  
+    }
+
+print "\t\t\t\t</ul>";
+print "\t\t\t</div>";
 ?>
-<div class="span3">
-  <ul class="sidebar-nav unstyled" style="position: fixed;">
-    <?php
-      foreach ($sections as $section) 
-      {
-        print '<div class="accordion-group">';
-        print '<div class="accordion-heading">';
-        print sprintf('<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion1"  href="#collapse%s">',$section->id);
-        print $section->uniqueSectionCode;
-        print '</a>';
-        print '</div>';
-        print sprintf('<div id="collapse%s" class="accordion-body collapse">',$section->id);
-        print '<div class="accordion-inner">';
-        print sprintf('<li><a href="students.php?sectionId=%s"><i class="icon-th"></i>Students</a></li>',$section->id);
-        print '<li><a href="#"><i class="icon-book"></i>Grades </a><span class="label label-important">NYI</span></li>';
-        print '<li><a href="#">Compare </a><span class="label label-important">NYI</span></li>';
-        print '<li><a href="#">Graded Assignments </a><span class="label label-important">NYI</span></li>';
-        print '</div>';
-        print '</div>';
-        print '</div>';  
-      }
-    ?>
-  </ul>
-</div>
