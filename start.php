36064021ccf5ea076f2a83d1ac158d85a2f0225a
Hunter Skrasek <hunterskrasek@me.com>
2013-03-06 13:54:58 -0600

    Last minute style change

--- start.php
+++ start.php
@@ -7,7 +7,28 @@ include 'header.php';
     <?php include 'sidebar.php'; ?>
     <div class="span9">
 		<div class="hero-unit">
-	        <h1>inComm!</h1>
+			<?php
+				$current_time = date(G); 
+				// echo $current_time;
+				$timeOfDay = "";
+				if ($current_time >= 0 && $current_time <= 12) 
+				{
+					$timeOfDay = "Good Morning, ";
+				}
+				else if($current_time >= 12 && $current_time <= 17)
+				{
+					$timeOfDay = "Good Afternoon, ";
+				}
+				else if($current_time >= 17 && $current_time <= 23)
+				{
+					$timeOfDay = "Good Evening, ";
+				}
+				else
+				{
+					$timeOfDay = "Welcome Back, ";
+				}
+			?>
+	        <h1><?php print $timeOfDay . $json->full_name?></h1>
 	        <p>Preventing failure through communication.</p>
       	</div>
     </div>
